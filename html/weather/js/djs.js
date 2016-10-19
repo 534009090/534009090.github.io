2887e15ccb90090fd67de651f1b3f78fdb5d87f3
xjak <534009090@qq.com>
2016-10-19 16:27:33 +0800

    Update djs.js

--- html/weather/js/djs.js
+++ html/weather/js/djs.js
@@ -17,25 +17,4 @@
     cityPicker.init();
 
 
-  $('#lookup').click(function(){ 
-       $.ajax({  
-        url:"https://wthrcdn.etouch.cn/weather_mini?city=上海",  
-        dataType:'jsonp',  
-        data:'',  
-        jsonp:'callback',  
-        success:function(result) {  
-               $('.header').css('display','block');
-             $('#Result').text(result.data.wendu+'℃'); 
-               $('#show').text(result.data.ganmao);  
-              $('#show1').text(result.data.forecast[0].fengxiang); 
-              $('#show2').text(result.data.forecast[0].fengli);
-              $('#show3').text(result.data.forecast[0].high);
-              $('#show4').text(result.data.forecast[0].type);
-              $('#show5').text(result.data.forecast[0].low);
-              $('#show0').text(result.data.forecast[0].date);
-            
-        },  
-        timeout:3000  
-    }); 
-        });
-
+  
